d21b0684493f4ac385b8451a9acf669a10ee3e1d
Nguyen Ngoc Khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2024-05-14 14:38:30 +0800

    auto_push

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
-DATA_FILE=/path/to/data/file
+DATA_FILE=data/enbm.src-tgt
 MODEL_NAME_OR_PATH=bert-base-multilingual-cased
 OUTPUT_FILE=data/out.txt
 
-CUDA_VISIBLE_DEVICES=0 awesome-align \
+CUDA_VISIBLE_DEVICES=0 python run_align.py \
     --output_file=$OUTPUT_FILE \
     --model_name_or_path=$MODEL_NAME_OR_PATH \
     --data_file=$DATA_FILE \
